5cc9f6faca7786e752cc1258d3f567fca929d646
discollizard <discollizard@github.com>
2023-05-10 13:37:03 -0700

    it responds, but loop exits because of bad file desciptor

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -10,14 +11,15 @@
 
 int main(int argc, char** argv){
 
-	int socket_descriptor;
-	int new_socket_descriptor_temp;
+	int server_fd, client_fd, line;
 	struct sockaddr_in address;
+	char line_buffer[LINE_BUFFER_SIZE];
+	char return_buffer[LINE_BUFFER_SIZE];
 
-	socket_descriptor = open_socket_or_die();
+	server_fd = open_socket_or_die();
 
 	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
 	address.sin_port = htons(PORT);
 
 	//ok, if this function fails, should the program just die?
@@ -26,25 +28,63 @@ int main(int argc, char** argv){
 	//maybe log the occurence and move on? idk, TODO
 	//
 
-	set_sock_opts_or_die(socket_descriptor);
+	set_sock_opts_or_die(server_fd);
 
-	bind_socket_or_die(socket_descriptor, &address);
+	bind_socket_or_die(server_fd, &address);
 
-	if (listen(socket_descriptor, 1024) < 0) {
+	if (listen(server_fd, 1024) < 0) {
 		perror("Error while listening to incoming connections");
 		exit(EXIT_FAILURE);
 	}
 
 	printf("Listening on port %d...", PORT);
 
-	if ((new_socket_descriptor_temp = accept(socket_descriptor, (struct sockaddr*)&address, (socklen_t*)sizeof((struct sockaddr*)&address))) < 0){
-		perror("Error while accepting incoming connection");
-		exit(EXIT_FAILURE);
+	for(;;){
+
+		struct sockaddr_in client_addr;
+		socklen_t client_address_len;
+
+		client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_address_len);
+
+		if (client_fd < 0){
+			perror("Error while accepting incoming connection");
+			close(server_fd);
+			exit(EXIT_FAILURE);
+		}
+
+		memset(line_buffer, 0, LINE_BUFFER_SIZE - 1);
+
+		printf("Connection accepted: %d", server_fd);
+
+		while((line = read(client_fd, line_buffer, LINE_BUFFER_SIZE - 1)) > 0){
+			//could log this in a file later
+			fprintf(stdout, "\n%s\n", line_buffer);
+			fflush(stdout);
+
+			if (line_buffer[line-1] == '\n'){
+				break;
+			}
+
+			memset(line_buffer, 0, LINE_BUFFER_SIZE - 1);
+		}
+		if(line < 0){
+			perror("Error while reading from socket");
+			close(server_fd);
+			exit(EXIT_FAILURE);
+		}
+
+		snprintf((char*)return_buffer, sizeof(return_buffer), "HTTP/1.0 200 OK \r\n\r\nAmo mickelly");
+
+		if((write(client_fd, (char*)return_buffer, strlen((char*)return_buffer))) < 0){
+			perror("Error while writing to socket");
+			close(server_fd);
+			exit(EXIT_FAILURE);
+		}
+
+		close(server_fd);
 	}
 
-	printf("Connection accepted: %d", new_socket_descriptor_temp);
 
-	close(socket_descriptor);
 	return 0;
 
 	//wait for socket connection 
--- params.h
+++ params.h
@@ -2,5 +2,6 @@
 #define PARAMS_H
 
 #define PORT 80
+#define LINE_BUFFER_SIZE 2048
 
 #endif
